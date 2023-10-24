bf50b223b0ffe49dc3f9e035ac6a27820fa8096a
Melodi Bayraktar <101207246+MelodiBayraktar@users.noreply.github.com>
2023-10-24 19:12:42 +0300

    first commit

---
+++ Strings/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Strings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Intro();
+        string sentence = "My name is Engin Demiroğ";
+        var result = sentence.Length;
+        Console.WriteLine(result);
+
+        var result2 = sentence.Clone();
+        sentence = "My name is Derin Demiroğ";
+        Console.WriteLine(result2);
+
+        bool result3 =  sentence.EndsWith("ğ");
+        bool result4 = sentence.StartsWith("ğ");
+
+        var result5 = sentence.IndexOf("namee");
+        Console.WriteLine(result5);
+
+        var result6 = sentence.IndexOf(" ");
+        Console.WriteLine(result6);
+
+        var result7 = sentence.LastIndexOf(" ");
+        Console.WriteLine(result7);
+
+        var result8 = sentence.Insert(0, "Hello ");
+        Console.WriteLine(result8);
+
+        var result9 = sentence.Substring(3,4);
+        Console.WriteLine(result9);
+
+        var result10 = sentence.ToLower();
+        Console.WriteLine(result10);
+
+        var result11 = sentence.ToUpper();
+        Console.WriteLine(result11);
+
+        var result12 = sentence.Replace(" ", "-");
+        Console.WriteLine(result12);
+
+        var result13 = sentence.Remove(2, 5);
+        Console.WriteLine(result13);
+
+        Console.ReadLine();
+    }
+    private static void Intro()
+    {
+        string city = "Ankara";
+        //Console.WriteLine(city[0]);
+        foreach (var i in city)
+        {
+            Console.WriteLine(i);
+        }
+
+        string city2 = "İstanbul";
+        string result = city + city2;
+        Console.WriteLine(result);
+
+        Console.WriteLine(String.Format("{0} {1}", city, city2));
+    }
+}
+
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TypesAndVariables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+    
+        int number1 = 2147483647;
+        Console.WriteLine("Number1 is {0}", number1);
+
+        long number2 = 9223372036854775807;
+        Console.WriteLine("Number2 is {0}", number2);
+
+        short number3 = 32767;
+        Console.WriteLine("Number3 is {0}", number3);
+
+        byte number4 = 255;
+        Console.WriteLine("Number4 is {0}", number4);
+
+        double number5 = 10.4;
+        Console.WriteLine("Number5 is {0}", number5);
+
+        decimal number6 = 10.4m;
+        Console.WriteLine("Number6 is {0}", number6);
+
+        var number7 = 10;
+        number7 = 'A';
+        //number7= "A";
+        Console.WriteLine("Number7 is {0}", number7);
+
+        bool condition = false;
+
+        char character = 'A';
+        Console.WriteLine("Character is : {0}", (int)character);
+
+        Console.WriteLine((int)Days.Friday);
+
+        Console.ReadLine();
+    }
+}
+enum Days
+{
+    Monday = 1, Tuesday, Wednesday, Friday, Saturday, Sunday
+}
+
